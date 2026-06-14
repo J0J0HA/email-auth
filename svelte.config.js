c4c7a5d54173be2850db5f61defe8144144d34e8
JoJoJux (aka. J0J0HA) <johannes@jojojux.de>
2026-06-14 17:56:31 +0200

    Fix csrf origin

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -18,7 +19,7 @@ const config = {
 		},
 		csrf: {
 			trustedOrigins: [
-				process.env.SCHEME + "://" + process.env.HOSTNAME,
+				process.env.ORIGIN,
 			]
 		},
 	},
